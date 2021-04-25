ed78780f82df3b30bbebe968fb84fa2240675846
Soubik Bhui <soubik.bhui16@gmail.com>
2021-04-25 13:05:44 +0530

    rmdir command cmpl

--- commands/register_all_commands.go
+++ commands/register_all_commands.go
@@ -4,11 +4,12 @@ import (
 	"github.com/soubikbhuiwk007/ghve/commands/exit" // command: exit
 	"github.com/soubikbhuiwk007/ghve/commands/ptoken" // command: get-token
 	"github.com/soubikbhuiwk007/ghve/commands/pwd" // command: pwd
-	"github.com/soubikbhuiwk007/ghve/commands/ls" // command: ls
-	"github.com/soubikbhuiwk007/ghve/commands/cd" // command: cd
+	"github.com/soubikbhuiwk007/ghve/commands/ls" // command: ls <repo|org|gist> <userid>
+	"github.com/soubikbhuiwk007/ghve/commands/cd" // command: cd <subdirectory>
 	"github.com/soubikbhuiwk007/ghve/commands/dir" // command: dir
-	"github.com/soubikbhuiwk007/ghve/commands/mkdir" // command: mkdir
-	"github.com/soubikbhuiwk007/ghve/commands/mkfile" // command: mkfile
+	"github.com/soubikbhuiwk007/ghve/commands/mkdir" // command: mkdir <> || <subdirectory>
+	"github.com/soubikbhuiwk007/ghve/commands/mkfile" // command: mkfile <> || <filename>
+	"github.com/soubikbhuiwk007/ghve/commands/rmdir" // command: rmdir <subdirectory>
 )
 
 func RegisterAll() {
@@ -20,4 +21,5 @@ func RegisterAll() {
 	dir.Register() // List Content inside Directory
 	mkdir.Register() // Create a new Directory/Repository
 	mkfile.Register() // Create a new File/Gist
+	rmdir.Register() // Remove an existing Directory/Repository
 }
