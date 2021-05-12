a29a3f65cabb7bb14893536d1b5c888abb51d9ae
Soubik Bhui <soubik.bhui16@gmail.com>
2021-05-12 18:40:04 +0530

    fix typo

--- commands/register_all_commands.go
+++ commands/register_all_commands.go
@@ -32,7 +32,7 @@ func RegisterAll() {
 	pwd.Register() // Print Working Directory
 	ptoken.Register() // Print Token
 	exit.Register() // Exit with Code 0
-	ls.Register() // List Organistion/Repository/Gist for Any User
+	ls.Register() // List Organisation/Repository/Gist for Any User
 	cd.Register() // Change Directory
 	dir.Register() // List Content inside Directory
 	mkdir.Register() // Create a new Directory/Repository
